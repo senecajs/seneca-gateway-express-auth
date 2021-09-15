76f6feddac6f581a61e0702be60424105b40b80e
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-09-15 09:13:13 +0300

    Allow the authentication request to be routed to a different Seneca instance

--- src/gateway-express-auth.ts
+++ src/gateway-express-auth.ts
@@ -2,12 +2,15 @@
 
 function gateway_express_auth(this: any, options: any) {
   const seneca: any = this
-  const root: any = seneca.root
+  const seneca_auth: any = get_auth_instance(seneca, options)
+
 
   seneca.act('sys:gateway,add:hook,hook:custom', {
     action: async (custom: any, _json: any, ctx: any) => {
       const token = ctx.req.cookies[options.cookie.name]
-      let res = await root.post('sys:user,auth:user', { token })
+        
+      const res = await seneca_auth.root.post('sys:user,auth:user', { token })
+
       if (res.ok) {
         // TODO: need a plugin, seneca-principal, to make this uniform
         custom.principal = { user: res.user }
@@ -17,12 +20,15 @@ function gateway_express_auth(this: any, options: any) {
 
   seneca.act('sys:gateway,add:hook,hook:action', {
     action: async function(this: any, _msg: any, ctx: any) {
-      let seneca: any = this
-      let user = seneca?.fixedmeta?.custom?.principal?.user
+      const seneca: any = this
+      const user = seneca?.fixedmeta?.custom?.principal?.user
+
       if (null == user) {
         ctx.res.sendStatus(401)
         return { done$: true }
       }
+
+      return null
     }
   })
 
@@ -31,6 +37,35 @@ function gateway_express_auth(this: any, options: any) {
     name: 'gateway-express-auth'
   }
 }
+
+
+function get_auth_instance(seneca: any, options: any) {
+  if (!seneca?.isSeneca) {
+    throw new Error('The seneca arg must be a Seneca instance')
+  }
+
+  const make_seneca_auth = options?.seneca_auth
+
+  if (null != make_seneca_auth &&
+    'function' !== typeof make_seneca_auth)
+  {
+    throw new Error('The `seneca_auth` option must be a function')
+  }
+
+
+  const seneca_auth: any = 'function' === typeof make_seneca_auth
+    ? make_seneca_auth()
+    : seneca
+
+  if (!seneca_auth?.isSeneca) {
+    throw new Error(
+      'The `seneca_auth` function must return a Seneca instance'
+    )
+  }
+
+  return seneca_auth
+}
+
 
 
 // Default options.
